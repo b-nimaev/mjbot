2ea727ccb5a2b67db333a75716277c2145f84b01
b-nimaev <moyhoroshenkiy@gmail.com>
2023-05-17 11:50:39 +0800

    update

--- src/database.ts
+++ src/database.ts
@@ -1,17 +1,12 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(`mongodb://${encodeURIComponent(<string>process.env.username?.replace(/"/g, ''))}:${encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))}}@87.236.22.124:27017/burlang?authSource=admin`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-} as any).catch(error => { console.error(error) });
-
+const connectionString = `mongodb://${encodeURIComponent(<string>process.env.username?.replace(/"/g, ''))}:${encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))}}@87.236.22.124:27017/burlang?authSource=admin`;
 const MONGODB_URI: string = `${process.env.MONGODB_URI}` || 'mongodb://localhost:27017/rlhub';
 
-mongoose.connect(MONGODB_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-} as any).catch(error => { console.error(error) });
-
-mongoose.connection.on('connected', () => {
-    console.log('Connected to MongoDB!');
-});
+mongoose.connect(connectionString || MONGODB_URI)
+    .then(() => {
+        console.log('Connected to MongoDB!');
+    })
+    .catch(error => {
+        console.error(error);
+    });
