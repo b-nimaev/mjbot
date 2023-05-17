68712f9bfbe093fb638b6ae6503ab820b5812aa7
b-nimaev <moyhoroshenkiy@gmail.com>
2023-05-17 17:20:02 +0800

    update

--- src/app.ts
+++ src/app.ts
@@ -89,3 +89,39 @@ app.use(cors());
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+const fetchData = async () => {
+    const { default: fetch } = await import('node-fetch');
+
+    const res = await fetch('http://localhost:4040/api/tunnels');
+    const json = await res.json();
+    console.log(json)
+    //@ts-ignore
+    const secureTunnel = json.tunnels[0].public_url
+    console.log(secureTunnel)
+    await bot.telegram.setWebhook(`${secureTunnel}${secretPath}`)
+        .then(res => {
+            console.log(res)
+        })
+};
+
+async function set_webhook() {
+    console.log(`${process.env.mode?.replace(/"/g, '')}`)
+    if (`${process.env.mode?.replace(/"/g, '')}` === "production") {
+        console.log(`${process.env.mode?.replace(/"/g, '')}`)
+        console.log(`secret path: ${secretPath}`)
+        await bot.telegram.setWebhook(`https://profori.pro/telegraf/secret_path`)
+            .then((status) => {
+                console.log(secretPath);
+                console.log(status);
+            }).catch(err => {
+                console.log(err)
+            })
+    } else {
+        await fetchData().catch((error: any) => {
+            console.error('Error setting webhook:', error);
+        });
+    }
+};
+
+set_webhook()
