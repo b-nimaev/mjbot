4fe376529c2221f508b3987d20c0479fd134931e
b-nimaev <moyhoroshenkiy@gmail.com>
2023-05-17 11:58:57 +0800

    updat

--- src/database.ts
+++ src/database.ts
@@ -1,6 +1,13 @@
 import mongoose from 'mongoose';
 
-const connectionString = `mongodb://${encodeURIComponent(<string>process.env.username?.replace(/"/g, ''))}:${encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))}}@87.236.22.124:27017/burlang?authSource=admin`;
+// @ts-ignore
+const username = encodeURIComponent('alexandr')
+const password = encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))
+
+console.log(username)
+console.log(password)
+
+const connectionString = `mongodb://${username}:${password}}@87.236.22.124:27017/burlang?authSource=admin`;
 const MONGODB_URI: string = `${process.env.MONGODB_URI}` || 'mongodb://localhost:27017/rlhub';
 
 mongoose.connect(connectionString || MONGODB_URI)
