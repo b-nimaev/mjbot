8bca934a288a4e9167ad4d9a676cbae6fd4760c1
b-nimaev <moyhoroshenkiy@gmail.com>
2023-05-17 11:34:46 +0800

    update

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,10 @@
 import mongoose from 'mongoose';
 
+mongoose.connect(`mongodb://${encodeURIComponent(<string>process.env.username?.replace(/"/g, ''))}:${encodeURIComponent(<string>process.env.password?.replace(/"/g, ''))}}@87.236.22.124:27017/burlang?authSource=admin`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+} as any).catch(error => { console.error(error) });
+
 const MONGODB_URI: string = `${process.env.MONGODB_URI}` || 'mongodb://localhost:27017/rlhub';
 
 mongoose.connect(MONGODB_URI, {
